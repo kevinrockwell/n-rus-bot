bce7e2eccaa73a62c32ec70d662bcb89027f80b2
Logan Haug <loganhaug@Logans-MacBook-Pro.local>
2020-04-15 19:57:30 -0700

    Add basic message event handler

--- n_rus_bot.py
+++ n_rus_bot.py
@@ -1,19 +1,29 @@
 # External imports
 import discord
 import logging
-import yaml
+import shlex
 # No internal imports
 
 # Set basic logging config
 logging.basicConfig(filename='discord.log', filemode='a', format='%(asctime)s %(message)s', level='ERROR')
 
-# Loads the yaml file
-with open('bot.yml', 'r') as file:
-    BOT_YAML = yaml.load(file, Loader=yaml.SafeLoader)
 # Initializes the discord bot client
 N_RUS_BOT = discord.Client()
+
+COMMANDS = {
+    'quote': [';quote', ';q']
+}
+
+@N_RUS_BOT.event
+async def on_message(message):
+    for alias in COMMANDS['quote']:
+        if message.content.startswith(alias):
+            content = shlex.split(message.content)
+            print(content)
+
 # Loads the bot token
-with open('token.txt', r) as file:
+with open('token.txt', 'r') as file:
     TOKEN = file.read()
 # Runs the bot
 N_RUS_BOT.run(TOKEN)
+print('asdmklaskldlkasdkl')
