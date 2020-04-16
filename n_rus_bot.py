ced39399d2e19a2be32689c60b127afc1fcd8146
Logan Haug <loganhaug@Logans-MacBook-Pro.local>
2020-04-15 19:36:07 -0700

    Fix being a dumb doo doo head

--- n_rus_bot.py
+++ n_rus_bot.py
@@ -7,17 +7,8 @@ import shlex
 # Set basic logging config
 logging.basicConfig(filename='discord.log', filemode='a', format='%(asctime)s %(message)s', level='ERROR')
 
-# Loads the yaml file
-with open('bot.yml', 'r') as file:
-    BOT_YAML = yaml.load(file, Loader=yaml.SafeLoader)
 # Initializes the discord bot client
 N_RUS_BOT = discord.Client()
-
-@N_RUS_BOT.event
-async def on_message(message):
-    if message.content.startswith(';q'):
-        content = shlex.split(message.content)
-
 
 # Loads the bot token
 with open('token.txt', 'r') as file:
