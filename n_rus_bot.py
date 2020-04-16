7967717c4c454ee13144ad875f4961922ba4a7a4
Logan Haug <loganhaug@Logans-MacBook-Pro.local>
2020-04-15 19:34:26 -0700

    Remove unused import in n_rus_bot.py

--- n_rus_bot.py
+++ n_rus_bot.py
@@ -1,7 +1,7 @@
 # External imports
 import discord
 import logging
-import yaml
+import shlex
 # No internal imports
 
 # Set basic logging config
@@ -12,6 +12,13 @@ with open('bot.yml', 'r') as file:
     BOT_YAML = yaml.load(file, Loader=yaml.SafeLoader)
 # Initializes the discord bot client
 N_RUS_BOT = discord.Client()
+
+@N_RUS_BOT.event
+async def on_message(message):
+    if message.content.startswith(';q'):
+        content = shlex.split(message.content)
+
+
 # Loads the bot token
 with open('token.txt', 'r') as file:
     TOKEN = file.read()
