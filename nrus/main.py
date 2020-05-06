5073466074e2605ab6085c2bca1c8c085b19bbc7
kevinrockwell <55220125+kevinrockwell@users.noreply.github.com>
2020-05-06 13:39:40 -0700

    Do paths relative to main directory

--- nrus/main.py
+++ nrus/main.py
@@ -1,15 +1,18 @@
 import json
 import logging
+import os
 import sys
 
 import bot
 
 # Set basic logging config
-logging.basicConfig(filename='../../discord.log', filemode='a', format='%(asctime)s %(message)s', level='ERROR')
+dir_name = os.path.split(__file__)[:-1]
+logging.basicConfig(filename=os.path.join(*dir_name, 'discord.log'), filemode='a',
+                    format='%(asctime)s %(message)s', level='ERROR')
 
 
 # Loads the bot token
-with open(sys.argv[1]) as file:
+with open(os.path.join(*dir_name, 'settings.json')) as file:
     settings = json.load(file)
 
 bot = bot.NRus(settings)
