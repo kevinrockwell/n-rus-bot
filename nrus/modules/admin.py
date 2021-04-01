d5e8c61a34a3caa4b238567d50a1c19855080847
kevinrockwell <55220125+kevinrockwell@users.noreply.github.com>
2021-03-31 17:46:26 -0700

    Hide admin commands from help message

--- nrus/modules/admin.py
+++ nrus/modules/admin.py
@@ -19,14 +19,14 @@ GIT_CHANGED_FILE: Pattern = re.compile(r'nrus/(.+?).py +\| +[1-9] +\+*-*')
 MODULE_PATH_MATCH: Pattern = re.compile(r'nrus/modules/[^/]+')
 
 
-class Admin(commands.Cog):
+class Admin(commands.Cog, command_attrs={'hidden': True}):
     def __init__(self, bot: NRus):
         self.bot = bot
 
     def cog_check(self, ctx):
         return self.bot.is_owner(ctx.author)
 
-    @commands.command(hidden=True)
+    @commands.command()
     async def reload(self, ctx: commands.Context, name: Optional[str] = None) -> None:
         if name:
             ext_name = f'modules.{name}'
@@ -36,25 +36,25 @@ class Admin(commands.Cog):
             await ctx.send('Reloading all modules...')
             self.bot.reload_extensions()
 
-    @commands.command(hidden=True)
+    @commands.command()
     async def load(self, ctx: commands.Context, name: str) -> None:
         ext_name = f'modules.{name}'
         await ctx.send(f'Loading {ext_name}')
         self.bot.load_extension(ext_name)
 
-    @commands.command(hidden=True)
+    @commands.command()
     async def unload(self, ctx: commands.Context, name: str) -> None:
         ext_name = f'modules.{name}'
         await ctx.send(f'Unloading {ext_name}')
         self.bot.unload_extension(ext_name)
 
-    @commands.command(hidden=True)
+    @commands.command()
     async def loaded(self, ctx: commands.Context) -> None:
         e: discord.Embed = utils.embed()
         e.add_field(name='Cogs Loaded:', value='\n'.join(self.bot.cogs))
         await ctx.send(embed=e)
 
-    @commands.command(hidden=True, name='exec')
+    @commands.command(name='exec')
     async def exec_(self, ctx: commands.Context, *, text) -> None:
         text = text.strip('```python').strip('`').split('\n')
         out = ['async def f():']
@@ -72,7 +72,7 @@ class Admin(commands.Cog):
         if result is not None:
             await ctx.send(result)
 
-    @commands.command(name='gitreload', hidden=True, aliases=['reloadgit'])
+    @commands.command(name='gitreload', aliases=['reloadgit'])
     async def git_reload(self, ctx: commands.Context):
         await ctx.send('Checking out from git...')
         output = await self.run_commands('git pull origin main')
